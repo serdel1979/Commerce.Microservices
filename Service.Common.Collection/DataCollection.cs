fa31caa24078f469663d32bb5c32f586467ec99f
sergio <sdlbsso@gmail.com>
2024-05-17 12:50:08 -0300

    empaquetado de extension pagging

--- Service.Common.Collection/DataCollection.cs
+++ Service.Common.Collection/DataCollection.cs
@@ -17,7 +17,7 @@ namespace Service.Common.Collection
         }
 
 
-        public IEnumerable<T> Items { get; set; }
+        public IEnumerable<T>? Items { get; set; }
         public int Total { get; set; }
         public int Page { get; set; }
         public int Pages { get; set; }
